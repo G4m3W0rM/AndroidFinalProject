268a0e34aa74ebd7a9365344b0d3a403f3a60036
ramsricharan <ramcharan.syr@gmail.com>
2017-11-28 19:32:12 -0500

    Just started

---
+++ SchoolDiaries/app/src/main/java/com/akhil/schooldiaries/Fragment_Login.java
@@ -0,0 +1,29 @@
+package com.akhil.schooldiaries;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ */
+public class Fragment_Login extends Fragment {
+
+    public Fragment_Login() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+}
