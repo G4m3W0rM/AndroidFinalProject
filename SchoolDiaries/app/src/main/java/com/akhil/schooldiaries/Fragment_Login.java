1b421f35026a19cb77fb9d5553a4f3451db771e4
ramsricharan <ramcharan.syr@gmail.com>
2017-11-28 22:36:02 -0500

    Login Activity parent teacher check added

--- SchoolDiaries/app/src/main/java/com/akhil/schooldiaries/Fragment_Login.java
+++ SchoolDiaries/app/src/main/java/com/akhil/schooldiaries/Fragment_Login.java
@@ -10,12 +10,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -30,6 +36,11 @@ public class Fragment_Login extends Fragment  implements View.OnClickListener{
     private Button Loginme;
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+
+
+    private RadioButton Radio_Teacher;
+    private RadioButton Radio_Parent;
 
 
     public Fragment_Login() {
@@ -45,6 +56,8 @@ public class Fragment_Login extends Fragment  implements View.OnClickListener{
 
         progressDialog = new ProgressDialog(getContext());
         firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
 
         userid = (EditText) view.findViewById(R.id.UsernameField);
         password = (EditText) view.findViewById(R.id.PasswordField);
@@ -52,15 +65,18 @@ public class Fragment_Login extends Fragment  implements View.OnClickListener{
         Loginme = (Button) view.findViewById(R.id.Button_Submit);
         Loginme.setOnClickListener(this);
 
+        Radio_Teacher = (RadioButton) view.findViewById(R.id.Radio_Teacher);
+        Radio_Parent = (RadioButton) view.findViewById(R.id.Radio_Parent);
+
         return view;
     }
 
 
     public void LoginPressed(View view) {
-        final String username = userid.getText().toString();
+        final String email = userid.getText().toString();
         String pass = password.getText().toString();
 
-        if (username.isEmpty()) {
+        if (email.isEmpty()) {
             Toast.makeText(getContext(), "User Id cannot be empty!", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -73,22 +89,60 @@ public class Fragment_Login extends Fragment  implements View.OnClickListener{
         progressDialog.setMessage("Loggin in...");
         progressDialog.show();
 
-        firebaseAuth.signInWithEmailAndPassword(username, pass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+        firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 progressDialog.dismiss();
                 if(task.isSuccessful())
                 {
-                    Toast.makeText(getContext(), "Welcome " + username , Toast.LENGTH_LONG).show();
-                    Intent i = new Intent(getActivity(),ParentMain.class);
-                    i.putExtra("username", username);
-                    startActivity(i);
-                    getActivity().finish();
+                    String uid = firebaseAuth.getCurrentUser().getUid();
+                    checkUserType(uid);
                 }
                 else
                 {
                     Toast.makeText(getContext(), "Loggin failed", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+    }
+
+
+    private void checkUserType(String uid)
+    {
+        final boolean result = false;
+
+        if(Radio_Teacher.isChecked())
+        {
+            Intent i = new Intent(getActivity(), ParentMain.class);
+            LoginAs("staff", uid, i);
+        }
+
+        else if (Radio_Parent.isChecked())
+        {
+            Intent i = new Intent(getActivity(), ParentMain.class);
+            LoginAs("parents", uid, i);
+        }
+    }
+
+
+    private void LoginAs(final String usertype, final String uid, final Intent i)
+    {
+
+        databaseReference.child(usertype).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild(uid)) {
+                    startActivity(i);
+                    getActivity().finish();
+                }
+                else
+                {
+                    Toast.makeText(getContext(), "You do not have access to " + usertype + " account", Toast.LENGTH_LONG).show();
+                    FirebaseAuth.getInstance().signOut();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
             }
         });
     }
